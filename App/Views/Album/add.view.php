e2577aba1b84ac6527b00f54092d699f15d591e4
Damián Konečný <konecny18@stud.uniza.sk>
2026-01-21 16:45:13 +0100

    pridane komentare do priecinka album

--- App/Views/Album/add.view.php
+++ App/Views/Album/add.view.php
@@ -4,7 +4,15 @@
 ?>
 
 <?php if (!empty($errors)): ?>
+    <!--
+        Zobrazenie validačných chýb
+        row – Bootstrap grid riadok
+        justify-content-center – centrovanie obsahu
+        -->
     <div class="row justify-content-center">
+        <!--
+        col-6 – stĺpec so šírkou 6/12
+        -->
         <div class="col-6">
             <?php foreach ($errors as $error): ?>
                 <div class="alert alert-danger" role="alert">
@@ -17,7 +25,15 @@
 
 <div class="row justify-content-center">
     <div class="col-6 d-flex gap-4 flex-column">
+        <!--
+        Nadpis stránky – vytvorenie nového albumu
+        -->
         <h1>Pridanie albumu</h1>
+        <!--
+        Znovupoužiteľný formulár
+        Rovnaký ako pri editácii albumu
+        (MVC princíp – DRY)
+        -->
         <?php require 'form.view.php' ?>
     </div>
 </div>
